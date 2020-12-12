15a98abd77c635399365fe7e975cc9df2cb7465c
marco bottoni <bottoni.mrc@gmail.com>
2020-12-12 01:09:10 +0100

    boolzapp 1.2.0

---
+++ js/script.js
@@ -0,0 +1,25 @@
+var contactList = new Vue ({
+  el: "#contactList",
+  data:{
+    contacts: [
+      {
+        name: "michele",
+        avatar:"https://png.pngtree.com/png-vector/20190704/ourmid/pngtree-businessman-user-avatar-free-vector-png-image_1538405.jpg"
+      },
+      {
+        name: "fabio",
+        avatar: "https://png.pngtree.com/png-vector/20190710/ourlarge/pngtree-user-vector-avatar-png-image_1541962.jpg"
+      },
+      {
+        name: "samuele",
+        avatar: "https://www.kindpng.com/picc/m/78-786207_user-avatar-png-user-avatar-icon-png-transparent.png"
+      },
+      {
+        name: "luisa",
+        avatar: "https://png.pngtree.com/png-clipart/20190516/original/pngtree-users-vector-icon-png-image_3725294.jpg"
+      }
+    ]
+
+    }
+  }
+)
