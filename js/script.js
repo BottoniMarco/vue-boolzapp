521823132c446318321fda72b4bde8ec5fd20728
marco bottoni <bottoni.mrc@gmail.com>
2020-12-14 10:33:55 +0100

    boolzapp 2.0.0

--- js/script.js
+++ js/script.js
@@ -18,8 +18,148 @@ var contactList = new Vue ({
         name: "luisa",
         avatar: "https://png.pngtree.com/png-clipart/20190516/original/pngtree-users-vector-icon-png-image_3725294.jpg"
       }
-    ]
+    ],
 
-    }
+  },
+  methods:{
+    showChat : function () {
+      messages.show = true;
+      console.log("click");
+    },
+
   }
-)
+})
+
+
+
+
+var messages = new Vue ({
+  el: "#messages",
+  data:{
+
+    userMessages:{
+        michele:[
+          {
+            user:"hai portato a spasso il cane?"
+          },
+          {
+            user:"gli hai dato da mangiare?"
+          },
+          {
+            correspondent:"tutto fatto!"
+          }
+
+        ],
+        fabio:[
+          {
+            user:"userMessages-fabio-1"
+          },
+          {
+            user:"userMessages-fabio-2"
+          },
+          {
+            correspondent:"correspondentMessages-fabio-1"
+          }
+        ],
+        samuele:[
+          {
+            user:"userMessages-sam-1"
+          },
+          {
+            user:"userMessages-sam-2",
+          },
+          {
+            correspondent:"correspondentMessages-sam-1"
+          }
+        ],
+        luisa:[
+          {
+            user:"userMessages-luisa-1"
+          },
+          {
+            user:"userMessages-luisa-2"
+          },
+          {
+            correspondent:"correspondentMessages-luisa-1"
+          }
+        ]
+      },
+    show: false,
+    id: "fabio",
+  },
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+[
+ {
+   michele:[
+     {
+       user:"hai portato a spasso il cane?"
+     },
+     {
+       user:"gli hai dato da mangiare?"
+     },
+     {
+       correspondent:"tutto fatto!"
+     }
+
+   ],
+   fabio:[
+     {
+       user:"userMessages-fabio-1"
+     },
+     {
+       user:"userMessages-fabio-2"
+     },
+     {
+       correspondent:"correspondentMessages-fabio-1"
+     }
+   ],
+   samuele:[
+     {
+       user:"userMessages-sam-1"
+     },
+     {
+       user:"userMessages-sam-2",
+     },
+     {
+       correspondent:"correspondentMessages-sam-1"
+     }
+   ],
+   luisa:[
+     {
+       user:"userMessages-luisa-1"
+     },
+     {
+       user:"userMessages-luisa-2"
+     },
+     {
+       correspondent:"correspondentMessages-luisa-1"
+     }
+   ]
+ },
+],
+*/
